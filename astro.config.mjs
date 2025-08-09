d0434be509c8df085053fec0b0da918b10e73eff
Daniel Henry <daniel.henry@gmail.com>
2025-08-08 20:02:14 -0400

    Allow referencing source root using tilde in sources, Markdown compilation, and module imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import path from 'node:path';
 
 // https://astro.build/config
 export default defineConfig({
@@ -43,4 +44,11 @@ export default defineConfig({
 			],
 		}),
 	],
+	vite: {
+		resolve: {
+			alias: {
+				'~': path.resolve('./src'), // "~/..." -> "./src/..." in a manner compatible with static builds
+			},
+		},
+	},
 });
